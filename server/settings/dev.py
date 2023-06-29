5ba34c8c47be3d5573fdcc0278bb36bfa5edff1a
taku820801 <takuchanpower@gmail.com>
2023-06-29 14:16:31 +0900

    コメント整理

--- server/settings/dev.py
+++ server/settings/dev.py
@@ -1,8 +1,23 @@
+# 開発環境用の設定
+
 from .base import *
 
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# for django-cors-headers
+CORS_ALLOWED_ORIGINS = (
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+)
+
+# templateで if debug を使えるようにするため、設定が必要
+INTERNAL_IPS = (
+    '127.0.0.1',
+)
