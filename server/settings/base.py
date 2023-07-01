28bec70f794a76ba13b76ffb8b50e5744ce48994
takushisato <takuchanpower@gmail.com>
2023-07-01 23:50:21 +0900

    設定変更

--- server/settings/base.py
+++ server/settings/base.py
@@ -25,8 +25,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'corsheaders',
     'django_vite',
     'api',
     'task',
