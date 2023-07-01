d982b38b1f8a52fe188f17d7398dfd6ed4ee70b1
takushisato <takuchanpower@gmail.com>
2023-07-02 00:09:42 +0900

    一旦Reactページを簡単に修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,11 @@
 import './App.css'
 import {Navigate, Route, Routes} from "react-router-dom";
-import {Index} from "./pages/Index";
-import {TaskDetail} from "./pages/TaskDetail";
-import {TaskForm} from "./pages/TaskForm";
 
 function App() {
   return (
-    <Routes>
-      <Route path="tasks">
-        <Route path="" element={<Index />} />
-        <Route path="registration" element={<TaskForm />} />
-        <Route path=":taskId" element={<TaskDetail />} />
-      </Route>
-
-      <Route path="*" element={<Navigate to="tasks" replace />} />
-    </Routes>
+      <div>
+          <h1>hello, react</h1>
+      </div>
   )
 }
 
